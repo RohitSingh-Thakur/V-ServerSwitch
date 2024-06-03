153df41b1a92d96b8e18540da0a2887d7abad509
RohitSingh <thakurrohitdsingh@gmail.com>
2024-06-03 14:55:17 +0530

    first commit

---
+++ src/main/java/com/v/VServerSwitchApplication.java
@@ -0,0 +1,13 @@
+package com.v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VServerSwitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VServerSwitchApplication.class, args);
+	}
+
+}
